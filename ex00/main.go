c2d7a8b678d7fcf5963a4a11c20300e795c73fd3
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-05-14 15:28:57 +0900

    Refactoring

--- ex00/main.go
+++ ex00/main.go
@@ -17,49 +17,10 @@ func main() {
 		os.Exit(1)
 	}
 	strs := piscine.SplitWhiteSpaces(string(data))
-	for _, str := range strs {
-		println(str)
-		println(len(str))
-		if len(str) != 16 || CountIf(str, '#') != 4 || CountIf(str, '.') != 12 {
-			os.Stderr.Write([]byte("Error\n"))
-			os.Exit(1)
-		}
-		if !is_valid_tetrimino(str) {
-			os.Stderr.Write([]byte("Error\n"))
-			os.Exit(1)
-		}
+	if !piscine.Is_valid_param(strs) {
+		os.Stderr.Write([]byte("Error\n"))
+		os.Exit(1)
 	}
-}
-
-func is_valid_tetrimino(m string) bool {
-	found := 0
-	for i, r := range m {
-		if r == '.' {
-			continue
-		}
-		if i > 1 && i%4 != 0 && m[i-1] == '#' {
-			found++
-		}
-		if i < 15 && m[i+1] == '#' {
-			found++
-		}
-		if i >= 4 && m[i-4] == '#' {
-			found++
-		}
-		if i < 12 && m[i+4] == '#' {
-			found++
-		}
-	}
-	return found == 6 || found == 8
-}
-
-func CountIf(s string, to_find rune) (result int) {
-	for _, r := range s {
-		if r == to_find {
-			result++
-		}
-	}
-	return
 }
 
 func getArgc(args []string) (l int) {
---
+++ ex00/vendor/piscine/is_valid_tetrimino.go
@@ -0,0 +1,46 @@
+package piscine
+
+func Is_valid_param(params []string) bool {
+	for _, str := range params {
+		println(str)
+		println(len(str))
+		if len(str) != 16 || CountIf(str, '#') != 4 || CountIf(str, '.') != 12 {
+			return false
+		}
+		if !is_valid_tetrimino(str) {
+			return false
+		}
+	}
+	return true
+}
+
+func is_valid_tetrimino(m string) bool {
+	found := 0
+	for i, r := range m {
+		if r == '.' {
+			continue
+		}
+		if i > 1 && i%4 != 0 && m[i-1] == '#' {
+			found++
+		}
+		if i < 15 && m[i+1] == '#' {
+			found++
+		}
+		if i >= 4 && m[i-4] == '#' {
+			found++
+		}
+		if i < 12 && m[i+4] == '#' {
+			found++
+		}
+	}
+	return found == 6 || found == 8
+}
+
+func CountIf(s string, to_find rune) (result int) {
+	for _, r := range s {
+		if r == to_find {
+			result++
+		}
+	}
+	return
+}
